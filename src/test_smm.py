9be568b299bec9b4c383ce5906d95d3b5336ddb4
libido <leonardovlibido@gmail.com>
2019-02-21 18:25:27 +0100

    add gale_shapley test, improve matching test

--- src/test_smm.py
+++ src/test_smm.py
@@ -4,9 +4,10 @@ from matching import Matching
 
 def test():
     test_is_stable()
+    test_gale_shapley()
 
 
-def test_is_stable():
+def init_preferences():
     men_ranks = {'abe': ['ada', 'bea', 'cee'],
                  'ben': ['bea', 'ada', 'cee'],
                  'che': ['bea', 'cee', 'ada']}
@@ -15,11 +16,24 @@ def test_is_stable():
                    'bea': ['ben', 'abe', 'che'],
                    'cee': ['che', 'ben', 'abe']
                    }
+    return men_ranks, women_ranks
 
-    # matcher = StableMarriageMatcher(men_ranks, women_ranks)
-    # matches = matcher.match()
+
+def test_gale_shapley():
+    men_ranks, women_ranks = init_preferences()
+    matcher = StableMarriageMatcher(men_ranks, women_ranks)
+    assert(matcher.matching.is_stable() is False)
+    matcher.find_matching()
+    assert(matcher.matching.is_stable() is True)
+    print("gale_shapley: OK!")
+
+
+def test_is_stable():
+    men_ranks, women_ranks = init_preferences()
 
     matches = Matching(men_ranks, women_ranks)
+    assert(matches.is_stable() is False)
+
     matches.match_pair('abe', 'ada')
     matches.match_pair('ben', 'bea')
     matches.match_pair('che', 'cee')
@@ -31,7 +45,7 @@ def test_is_stable():
     matches.match_pair('che', 'cee')
     assert(matches.is_stable() is False)
 
-    print("OK!")
+    print("is_stable: OK!")
 
 
 
